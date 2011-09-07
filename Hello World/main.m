d61b022d2c5a4f949db61fdb37e99944b560b10d
Sam <sbrown2594@gmail.com>
2011-09-07 01:22:37 -0400

    Default Starting Point App

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Sam Brown on 9/7/11.
+//  Copyright 2011 UVM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
